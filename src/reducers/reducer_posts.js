14779fc7313c1e942435f8a3bb30bc1348a98996
Colin McNamara <coolinmc6@gmail.com>
2017-05-31 10:23:51 -0400

    Content

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,10 +5,7 @@ const intro = [
 		id: 1, 
 		title: 'Welcome!', 
 		tag: "intro", 
-		content: "Welcome to Colins React Blog App!  This basic app allows the user to add \"blog\" posts using Redux and ReduxForm\
-		 and to navigate around the application using React-Router. Please take a look around: create a post, delete a post, navigate\
-		 around, you know the drill.  When you want to learn more, click the 'About' link in the top right and reach out.  Thanks and I\
-		  look forward to talking to you."
+		content: "Welcome to Colins React Blog App!  This basic app allows the user to add \"blog\" posts using Redux and ReduxForm and to navigate around the application using React-Router. Please take a look around: create a post, delete a post, navigate around, you know the drill.  When you want to learn more, click the 'About' link in the top right and reach out.  Thanks and I  look forward to talking to you."
 	}
 ]
 
